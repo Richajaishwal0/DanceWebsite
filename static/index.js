e9a1e34f429cffb8d7c1607185eb6fedf798fb46
richajaishwal0 <richajaiswalhome@gmail.com>
2025-01-08 16:28:18 +0530

    added forms

---
+++ static/index.js
@@ -0,0 +1,23 @@
+let apply=document.querySelector(".apply");
+function showForm()
+{
+    if (apply.style.display=="none")
+    {
+        apply.style.display="block";
+    }
+    else{
+        apply.style.display="none";
+    }
+
+}
+function Greet()
+{   
+    if (apply.style.display=="none")
+    {
+        apply.style.display="block";
+    }
+    else{
+        apply.style.display="none";
+    }
+    alert("Thanks for filling up the form. We will contact you!!")
+}
